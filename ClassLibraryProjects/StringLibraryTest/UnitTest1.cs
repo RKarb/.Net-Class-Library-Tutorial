3d8e955ef02c70c1e1e63b9559ae93e0031a39b2
RKarb <36237554+RKarb@users.noreply.github.com>
2021-06-12 14:48:48 +0100

    .Net Testing of Class Library
    A test unit project has been created using the MSTest framework, in order to test the ClassLibrary project. Three test methods were added to check the following: whether words in an array starts with uppercase, words start with lower case and if the input is empty.

---
+++ ClassLibraryProjects/StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            // Tests that are expected to return true
+            string[] words = { "Alphabet", "Zebra", "Abc", "Αθήνα", "Москва" };
+
+            foreach(var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsTrue(result, String.Format("Expected for '{0}': false; actual: {1}", word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            // Tests that are expected to return false
+            string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство", "1234", ".", ";", " " };
+
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result, String.Format("Expected for '{0}': false; actual: {1}", word, result));
+            }
+        }
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            // Tests that are expected to return false
+            string[] words = { string.Empty, null };
+            foreach(var word in words)
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.IsFalse(result, String.Format("Expected for '{0}': false; actual: {1}", word == null ? "<null>" : word, result));
+            }
+        }
+    }
+}
